b8142abaa2ae4ec74674e8f705e1800fe762e6ab
Marcel Klehr <mklehr@gmx.net>
2026-02-12 12:23:59 +0100

    fix(ci): Use commands to setup accounts
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

---
+++ lib/Command/SetupUser.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\Dropbox\Command;
+
+use OCA\Dropbox\AppInfo\Application;
+use OCA\Dropbox\Service\SecretService;
+use OCP\Config\IUserConfig;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SetupUser extends Command {
+	public function __construct(
+		private SecretService $secretService,
+		private IUserConfig $userConfig,
+	) {
+		parent::__construct();
+	}
+
+	/**
+	 * Configure the command
+	 *
+	 * @return void
+	 */
+	protected function configure() {
+		$this->setName('integration_dropbox:setup-user')
+			->addArgument('userId', InputArgument::REQUIRED)
+			->addArgument('accountId', InputArgument::REQUIRED)
+			->addArgument('refresh_token', InputArgument::REQUIRED)
+			->setDescription('Setup the client credentials');
+	}
+
+	/**
+	 * Execute the command
+	 *
+	 * @param InputInterface $input
+	 * @param OutputInterface $output
+	 *
+	 * @return int
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output): int {
+		try {
+			$this->userConfig->setValueString($input->getArgument('userId'), Application::APP_ID, 'account_id', $input->getArgument('accountId'), lazy: true);
+			$this->secretService->setEncryptedUserValue($input->getArgument('userId'), 'refresh_token', $input->getArgument('refresh_token'));
+			$this->secretService->setEncryptedUserValue($input->getArgument('userId'), 'token', 'abc');
+		} catch (\Exception $ex) {
+			$output->writeln('<error>Failed to setup client credentials</error>');
+			$output->writeln($ex->getMessage());
+			return 1;
+		}
+
+		return 0;
+	}
+}
